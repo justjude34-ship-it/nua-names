4d2ab156b808f09ee7b26f97a45b7980d06ae226
justjude34-ship-it <justjude34@gmail.com>
2026-09-17 09:20:28 +1000

    Add comprehensive girls and boys name book with All/Girls/Boys search

---
+++ names.js
@@ -0,0 +1,87 @@
+/* Nua name book — girls, boys, and unisex.
+   Australian-weighted (2025 popular + classics + names common across AU communities). */
+window.NUA_NAMES = {
+  girl: ["Aaliyah","Abigail","Ada","Adelaide","Adeline","Adriana","Aisha","Alana","Alba","Alexandra","Alice","Alicia","Alison","Alma","Alyssa","Amara","Amelia","Amelie","Anastasia","Andrea","Angela","Angelina","Anika","Anita","Anna","Annabelle","Annalise","Annie","Anya","Aoife","April","Arabella","Aria","Ariana","Ariella","Arlette","Astrid","Athena","Aubrey","Audrey","Aurora","Autumn","Ava","Avery","Ayla","Beatrice","Bella","Bernadette","Beth","Bethany","Bianca","Billie","Blair","Bonnie","Bridget","Bronte","Brooke","Caitlin","Callie","Camilla","Camille","Caoimhe","Cara","Carmen","Caroline","Cassandra","Catherine","Cecilia","Celeste","Celine","Charlie","Charlotte","Chelsea","Chloe","Christina","Claire","Clara","Clarissa","Clementine","Cleo","Coco","Colette","Connie","Cora","Coraline","Cordelia","Cosima","Crystal","Dahlia","Daisy","Dakota","Daniella","Danielle","Daphne","Davina","Delilah","Della","Diana","Dolly","Dorothy","Eden","Edith","Edwina","Eleanor","Elena","Eliana","Elise","Eliza","Elizabeth","Ella","Elle","Ellie","Elodie","Eloise","Elsa","Elsie","Emilia","Emily","Emma","Esme","Estelle","Esther","Eva","Evangeline","Eve","Evelyn","Evie","Faith","Farrah","Fatima","Felicity","Fleur","Flora","Florence","Frances","Francesca","Frankie","Freya","Gabriella","Gemma","Genevieve","Georgia","Georgie","Gia","Gianna","Giselle","Gloria","Grace","Greta","Gwen","Gwendolyn","Hadley","Hailey","Hallie","Hanna","Hannah","Harley","Harlow","Harper","Harriet","Hart","Haven","Hayley","Hazel","Heidi","Helena","Henrietta","Hester","Holly","Hope","Imogen","India","Ingrid","Iris","Isabel","Isabella","Isabelle","Isla","Isobel","Ivana","Ivy","Jade","Jane","Janet","Janice","Jasmine","Jayla","Jean","Jessica","Jewel","Jillian","Joanna","Jocelyn","Johanna","Josephine","Josie","Joy","Joyce","Judith","Julia","Juliana","Julie","Juliet","June","Juniper","Kaia","Kaiya","Kara","Karen","Kate","Katerina","Katherine","Kathleen","Katie","Katrina","Kayla","Kaylee","Keira","Kelsey","Kendall","Kendra","Kennedy","Kenzie","Kiera","Kimberly","Kira","Kitty","Kylie","Lacey","Lainey","Lara","Laura","Laurel","Lauren","Layla","Leah","Leila","Lena","Leona","Leonie","Lila","Lilah","Liliana","Lillian","Lily","Lina","Linda","Lisa","Livia","Lola","Lorelai","Lorraine","Lottie","Louise","Lucia","Lucille","Lucy","Luna","Lydia","Lyla","Lyra","Mabel","Mackenzie","Macy","Madeline","Madison","Mae","Maeve","Maggie","Maisie","Makayla","Mandy","Mara","Marcella","Margot","Maria","Mariah","Marie","Marina","Marion","Marley","Martha","Martina","Mary","Matilda","Maya","Melanie","Melissa","Melody","Mercy","Mia","Michaela","Michelle","Mila","Mildred","Millie","Mina","Minnie","Miranda","Miriam","Molly","Mona","Monica","Morgan","Nadia","Nadine","Nancy","Naomi","Natalia","Natalie","Natasha","Nell","Niamh","Nicola","Nicole","Nina","Noelle","Nora","Norah","Nova","Nyla","Olive","Olivia","Opal","Ophelia","Orla","Paige","Paisley","Palmer","Pamela","Patricia","Paula","Pauline","Penelope","Penny","Petra","Philippa","Phoebe","Pia","Piper","Pippa","Poppy","Primrose","Priscilla","Quinn","Rachel","Raquel","Rebecca","Reese","Regina","Remi","Renee","Rhea","Riley","Rita","River","Roberta","Robin","Romy","Rosa","Rosalie","Rosalind","Rose","Rosemary","Rosie","Rowan","Ruby","Ruth","Sabrina","Sadie","Sage","Sally","Samantha","Sana","Sandra","Saoirse","Sara","Sarah","Sasha","Savannah","Scarlett","Selena","Serena","Shakira","Shannon","Sharon","Sheila","Sienna","Sierra","Simone","Skye","Sofia","Sonia","Sophia","Sophie","Stella","Stephanie","Summer","Susan","Susannah","Sydney","Sylvia","Tahlia","Talia","Tamara","Tamsin","Tanya","Tara","Tatiana","Taylor","Tessa","Thea","Theodora","Theresa","Tiana","Tiffany","Tilly","Toni","Tori","Tracy","Trinity","Uma","Ursula","Valentina","Valerie","Vanessa","Vera","Veronica","Victoria","Violet","Virginia","Vita","Vivienne","Waverley","Wendy","Whitney","Willa","Willow","Winifred","Winnie","Winter","Xanthe","Yara","Yasmin","Yasmine","Yuna","Yvette","Yvonne","Zara","Zelda","Zinnia","Zoe","Zoey","Zora","Zuri"],
+  boy: ["Aaron","Abdallah","Abdul","Abraham","Ace","Adam","Adrian","Aiden","Albert","Alec","Alejandro","Alex","Alexander","Alfie","Alfred","Ali","Alistair","Allen","Amir","Amos","Anderson","Andre","Andrei","Andrew","Angelo","Angus","Anthony","Anton","Antonio","Archer","Archie","Arjun","Arlo","Arthur","Asher","Atlas","Atticus","Austin","Avery","Axel","Bailey","Barnaby","Barry","Bartholomew","Beau","Beckett","Ben","Benedict","Benjamin","Bennett","Benson","Bernard","Bill","Billy","Blake","Bo","Bob","Boden","Bodhi","Boris","Bowen","Bradley","Brandon","Brant","Brayden","Brendan","Brennan","Brian","Brody","Bruce","Bruno","Bryan","Byron","Caleb","Callum","Calvin","Cameron","Carl","Carlos","Carter","Casey","Casper","Cassius","Charles","Charlie","Chester","Christian","Christopher","Clarence","Clark","Claude","Clayton","Clifford","Clive","Clyde","Cody","Cohen","Cole","Colin","Connor","Conrad","Cooper","Corbin","Corey","Craig","Cruz","Cyrus","Damian","Damien","Damon","Daniel","Danny","Dante","Darcy","Darren","Darryl","Dashiell","David","Dean","Declan","Denis","Dennis","Derek","Desmond","Dexter","Diego","Dominic","Donald","Donovan","Douglas","Drake","Drew","Duke","Duncan","Dustin","Dylan","Eddie","Eden","Edgar","Edmund","Edward","Edwin","Ehsan","Eli","Elias","Elijah","Elio","Elliot","Elliott","Ellis","Elmer","Elvis","Emanuel","Emil","Emilio","Emmanuel","Enzo","Eric","Ernest","Ethan","Eugene","Evan","Everett","Ewan","Ezra","Felix","Fergus","Fernando","Finlay","Finley","Finn","Fletcher","Flynn","Ford","Forest","Forrest","Francis","Frank","Franklin","Fraser","Fred","Freddie","Frederick","Gabriel","Gareth","Gary","Gavin","Gene","Geoffrey","George","Gerald","Gerard","Gideon","Glen","Glenn","Gordon","Graham","Grant","Gregory","Griffin","Hamish","Hank","Hans","Harley","Harold","Harrison","Harry","Harvey","Hassan","Hayden","Heath","Hector","Hendrix","Henry","Herbert","Herman","Heston","Holden","Houston","Howard","Hudson","Hugh","Hugo","Hunter","Hussein","Ian","Ibrahim","Idris","Ignatius","Igor","Ike","Imran","Indiana","Inigo","Ira","Isaac","Isaiah","Ishaan","Ivan","Ivor","Jack","Jackson","Jacob","Jagger","Jake","James","Jamie","Jared","Jarrod","Jason","Jasper","Jaxon","Jaxson","Jay","Jayden","Jean","Jed","Jedidiah","Jeff","Jeffrey","Jem","Jeremy","Jerome","Jesse","Jessie","Jim","Jimmy","Joaquin","Joe","Joel","Joey","John","Johnny","Jonah","Jonas","Jonathan","Jordan","Jorge","Jose","Joseph","Joshua","Josiah","Joss","Juan","Judah","Jude","Jules","Julian","Julio","Julius","Justin","Kaden","Kai","Kairo","Kaleb","Kane","Karim","Karl","Kash","Kasper","Keith","Ken","Kenneth","Kenny","Kevin","Kian","Kieran","Killian","King","Kingsley","Kirk","Kit","Koa","Kobe","Kurt","Kyle","Lachlan","Lance","Landon","Lane","Larry","Lawrence","Lawson","Lee","Leo","Leon","Leonard","Leonardo","Leonidas","Leroy","Levi","Lewis","Liam","Lincoln","Lionel","Lloyd","Logan","Lorenzo","Louie","Louis","Luca","Lucas","Luis","Luka","Luke","Lyndon","Lysander","Magnus","Malcolm","Malik","Manuel","Marc","Marcel","Marco","Marcus","Mario","Mark","Marley","Marshall","Martin","Marvin","Mason","Mateo","Matias","Matt","Matthew","Matthias","Maurice","Max","Maximilian","Maxwell","Micah","Michael","Miguel","Milan","Miles","Miller","Milo","Milton","Mitchell","Mohammed","Mohan","Monte","Montgomery","Morgan","Morris","Moses","Muhammad","Murphy","Myles","Nash","Nathan","Nathaniel","Ned","Neil","Nelson","Niall","Nicholas","Nick","Nico","Nicolas","Nigel","Noah","Nolan","Norman","Oliver","Omar","Orlando","Oscar","Oswald","Otis","Otto","Owen","Pablo","Paddy","Parker","Patrick","Paul","Pedro","Percival","Percy","Perry","Peter","Peyton","Philip","Phoenix","Pierce","Pierre","Porter","Preston","Prince","Quentin","Quinn","Quinton","Rafael","Ralph","Ramon","Randall","Randy","Raul","Raven","Ray","Raymond","Reed","Reese","Reid","Remy","Reuben","Rex","Rhys","Ricardo","Richard","Rick","Riley","River","Rob","Robert","Robin","Rocco","Rodney","Roger","Roland","Roman","Ron","Ronald","Rory","Ross","Rowan","Roy","Ruben","Russell","Ryan","Ryder","Sam","Samson","Samuel","Sander","Santiago","Santino","Saul","Sawyer","Scott","Sean","Sebastian","Sergio","Seth","Shane","Shawn","Sidney","Silas","Simon","Solomon","Sonny","Spencer","Stanley","Stefan","Stephen","Steve","Steven","Stewart","Stirling","Sullivan","Sven","Sydney","Sylvester","Tate","Taylor","Ted","Teddy","Terence","Terry","Thaddeus","Theo","Theodore","Thomas","Timothy","Titus","Tobias","Toby","Todd","Tom","Tomas","Tommy","Tony","Travis","Trent","Trevor","Tristan","Troy","Tyler","Tyrone","Tyson","Ulysses","Uriah","Victor","Vincent","Virgil","Vito","Wade","Walker","Wallace","Walter","Warren","Wayne","Wesley","Westley","Wilbur","Will","William","Willie","Wilson","Winston","Wyatt","Xavier","Yahya","Yasir","Yusuf","Yves","Zachariah","Zachary","Zack","Zaid","Zain","Zane","Zayn","Zion"],
+  uni: ["Alex","Alexis","Ariel","Avery","Bailey","Blake","Cameron","Casey","Charlie","Dakota","Eden","Emerson","Finley","Frankie","Harley","Hayden","Indigo","Jamie","Jordan","Kai","Lennox","Marley","Morgan","Parker","Phoenix","Quinn","Reese","Remi","Riley","River","Robin","Rowan","Sage","Sam","Skyler","Taylor"]
+};
+
+window.nuaNameBook = function (els) {
+  if (!els || !els.search || !els.suggest) return;
+  var book = window.NUA_NAMES || { girl: [], boy: [], uni: [] };
+  var gender = "all";
+  var LIMIT = 40;
+
+  function pool() {
+    if (gender === "girl") return book.girl.slice();
+    if (gender === "boy") return book.boy.slice();
+    var seen = {};
+    var out = [];
+    book.girl.concat(book.uni, book.boy).forEach(function (n) {
+      if (!seen[n]) { seen[n] = 1; out.push(n); }
+    });
+    return out.sort();
+  }
+  function mark(name) {
+    var g = book.girl.indexOf(name) >= 0;
+    var b = book.boy.indexOf(name) >= 0;
+    if (g && b) return "unisex";
+    if (book.uni.indexOf(name) >= 0) return "unisex";
+    return g ? "girl" : "boy";
+  }
+  function render() {
+    var q = (els.search.value || "").trim().toLowerCase();
+    var all = pool();
+    var hits = q
+      ? all.filter(function (n) { return n.toLowerCase().indexOf(q) === 0 || n.toLowerCase().indexOf(q) >= 0; })
+          .sort(function (a, b) {
+            var as = a.toLowerCase().indexOf(q) === 0 ? 0 : 1;
+            var bs = b.toLowerCase().indexOf(q) === 0 ? 0 : 1;
+            return as - bs || a.localeCompare(b);
+          })
+      : all;
+    if (els.meta) {
+      var label = gender === "girl" ? "girls" : gender === "boy" ? "boys" : "names";
+      els.meta.textContent = hits.length + " " + label + " in the book" + (q ? " matching \u201c" + els.search.value.trim() + "\u201d" : "") + ". Tap one to try it.";
+    }
+    var show = hits.slice(0, LIMIT);
+    els.suggest.innerHTML = show.map(function (n) {
+      return "<button class=\"chip " + mark(n) + "\" type=\"button\" data-name=\"" + n + "\">" + n + "</button>";
+    }).join("") + (hits.length > LIMIT ? "<span class=\"hint\">" + (hits.length - LIMIT) + " more — keep typing</span>" : "");
+  }
+  els.suggest.addEventListener("click", function (e) {
+    var btn = e.target.closest("[data-name]");
+    if (!btn) return;
+    if (els.first) {
+      els.first.value = btn.getAttribute("data-name");
+      els.first.dispatchEvent(new Event("input", { bubbles: true }));
+    }
+    els.search.value = btn.getAttribute("data-name");
+    render();
+  });
+  els.search.addEventListener("input", render);
+  if (els.filters) {
+    els.filters.addEventListener("click", function (e) {
+      var btn = e.target.closest("[data-book]");
+      if (!btn) return;
+      gender = btn.getAttribute("data-book") || "all";
+      Array.prototype.forEach.call(els.filters.querySelectorAll("[data-book]"), function (b) {
+        b.classList.toggle("on", b === btn);
+      });
+      render();
+    });
+  }
+  render();
+  window.__nuaBookWired = true;
+};
+
+document.addEventListener("DOMContentLoaded", function () {
+  if (window.__nuaBookWired) return;
+  window.nuaNameBook({
+    search: document.getElementById("search"),
+    suggest: document.getElementById("suggest"),
+    first: document.getElementById("first"),
+    meta: document.getElementById("bookMeta"),
+    filters: document.getElementById("genderFilters")
+  });
+});
